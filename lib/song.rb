37cb0a119c624cff8b5d45662986f675357b2e75
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-11 21:24:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,11 @@
 class Song
 
     @@number_of_songs = 0
+    @@artists_of_songs = []
+    @@genres_of_songs = []
+
+    attr_accessor :name :artist :genre
+
+    def initialize(name, song, genre)
 
 end
